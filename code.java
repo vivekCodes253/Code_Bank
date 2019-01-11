a82f4e53cde2cba9b6d689f8202006d9c2c85762
Vivek <viveknarayanan15082@cse.ssn.edu.in>
2019-01-11 07:49:15 +0530

    no 2

---
+++ code.java
@@ -0,0 +1,35 @@
+/* Code to efficiently multiply matrices, using transposed matrix
+*/
+
+import java.util.Scanner;
+
+class code
+{
+
+    public static int[][] transpose(int[][] m, int x, int y)
+    {
+
+    }
+
+    public static int[][] matmul(int[][] a,int b[][], int m, int n, int p)
+    {
+
+    }
+
+
+    public static void test()
+    {
+
+
+
+
+    }
+
+
+
+    public static void main(String[] args)
+    {
+        test();
+    }
+
+}
