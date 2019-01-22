3deacb216e3b88c5bd2f4dc67d9b00b6c9f6eff8
Vivek <viveknarayanan15082@cse.ssn.edu.in>
2019-01-22 08:40:14 +0530

    minor_formatting

--- template.java
+++ template.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 
 class
 {
-     /*  
-        Function Name   : sop
-        Purpose         : Simplifies System.out.print()
-        Input           : String to output
-        Return          : --
-    */
+
+    /*Function Name : sop
+    Purpose         : Simplifies System.out.print()
+    Input           : String to output
+    Return          : --    */
     public static void sop(String s){System.out.print(s);}
 
+
+    /*Function Name : 
+    Purpose         : 
+    Input           : 
+    Return          :     */
     public static void test(){}
 
     public static void main(String[] args)
