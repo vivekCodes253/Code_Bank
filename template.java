f8d25839328741b1d4d13e1144776831fd93fc13
Vivek <viveknarayanan15082@cse.ssn.edu.in>
2019-01-22 08:28:07 +0530

    comment_update

--- template.java
+++ template.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 class
 {
-    /*  
-        Function Name :
-        Purpose :
-        Input :
-        Return :
+     /*  
+        Function Name   : sop
+        Purpose         : Simplifies System.out.print()
+        Input           : String to output
+        Return          : --
     */
     public static void sop(String s){System.out.print(s);}
 
