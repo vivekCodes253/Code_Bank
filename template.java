3cc2c55420869258e726256a2b572ca6dfe9fcba
Vivek <viveknarayanan15082@cse.ssn.edu.in>
2019-01-22 08:25:06 +0530

    comment_to_template

--- template.java
+++ template.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 class
 {
+    /*  
+        Function Name :
+        Purpose :
+        Input :
+        Return :
+    */
     public static void sop(String s){System.out.print(s);}
 
     public static void test(){}
